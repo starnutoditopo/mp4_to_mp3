26d64a7502d0760a9a05a96d29dedc310fb51426
Starnuto di topo <starnutoditopo@gmail.com>
2023-09-29 17:31:49 +0200

    1st commit

---
+++ Mp4ToMp3.py
@@ -0,0 +1,32 @@
+#!/usr/local/bin/python
+
+import glob, sys, getopt, os
+from moviepy.editor import AudioFileClip
+
+def main(argv):
+    input_files = []
+    output_directory = ""
+    opts, _ = getopt.getopt(argv, "hi:o:", ["ifile=", "odir="])
+    for opt, arg in opts:
+        if opt == "-h":
+            print("test.py -i <input_files> -o <output_directory>")
+            sys.exit()
+        elif opt in ("-i", "--ifiles"):
+            input_files = glob.glob(arg)
+        elif opt in ("-o", "--odir"):
+            output_directory = arg
+    print("Input files are: ", input_files)
+    print("Output directory is: ", output_directory)
+
+    for input_file in input_files:
+        _, file_name = os.path.split(input_file)
+
+        base_name = os.path.splitext(file_name)[0]
+        new_file_name = base_name + '.mp3'        
+        output_file = os.path.join(output_directory, new_file_name)        
+        print(f"{output_file}")
+        with AudioFileClip(input_file) as clip:
+            clip.write_audiofile(output_file)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
