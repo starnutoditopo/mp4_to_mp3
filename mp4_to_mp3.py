6c391cbbf52a6a1912943699e1f0e8b51a4fedf7
Starnuto di topo <starnutoditopo@gmail.com>
2023-10-01 10:08:56 +0200

    comment out useless lines; fix help text

--- mp4_to_mp3.py
+++ mp4_to_mp3.py
@@ -16,14 +16,14 @@ def main(argv):
     opts, _ = getopt.getopt(argv, "hi:o:", ["ifile=", "odir="])
     for opt, arg in opts:
         if opt == "-h":
-            print("test.py -i <input_files> -o <output_directory>")
+            print("mp4_to_mp3.py -i <input_files> -o <output_directory>")
             sys.exit()
         elif opt in ("-i", "--ifiles"):
             input_files = glob.glob(arg)
         elif opt in ("-o", "--odir"):
             output_directory = arg
-    print("Input files are: ", input_files)
-    print("Output directory is: ", output_directory)
+    #print("Input files are: ", input_files)
+    #print("Output directory is: ", output_directory)
 
     for input_file in input_files:
         _, file_name = os.path.split(input_file)
@@ -31,7 +31,7 @@ def main(argv):
         base_name = os.path.splitext(file_name)[0]
         new_file_name = base_name + ".mp3"
         output_file = os.path.join(output_directory, new_file_name)
-        print(f"{output_file}")
+        #print(f"{output_file}")
         with AudioFileClip(input_file) as clip:
             clip.write_audiofile(output_file)
 
